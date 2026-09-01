cfc6739ef818004460da8a8ff2caa44a1dd5a360
vrund23 <142259152+vrund23@users.noreply.github.com>
2026-09-01 22:06:46 +0530

    test3

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '8.8.4.4']);
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
